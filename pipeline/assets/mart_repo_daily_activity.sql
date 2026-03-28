6b86544c98199bf80c3af0716fc063489aa08949
Roxanna Rodriguez <roxanna.rdgz@gmail.com>
2026-03-28 18:24:38 +0300

    add repo activity and event type marts

---
+++ pipeline/assets/mart_repo_daily_activity.sql
@@ -0,0 +1,16 @@
+/* @bruin
+name: mart_repo_daily_activity
+type: duckdb.sql
+materialization:
+  type: table
+@bruin */
+
+SELECT
+    repo_name,
+    DATE(created_at) AS event_date,
+    COUNT(*) AS total_events,
+    COUNT(DISTINCT actor_login) AS unique_actors
+FROM stg_selected_events
+GROUP BY
+    repo_name,
+    DATE(created_at);
